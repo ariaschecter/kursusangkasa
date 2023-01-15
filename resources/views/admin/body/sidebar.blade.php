a72e086e97c9794daff5c9813b3f974df1472c8f
ariaschecter <aria.maulana.m@gmail.com>
2023-01-15 19:15:00 +0700

    Initial commit

---
+++ resources/views/admin/body/sidebar.blade.php
@@ -0,0 +1,42 @@
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!-- User details -->
+        <div class="user-profile text-center mt-3">
+            <div class="">
+                <img src="{{ asset('backend/assets/images/users/logo-admin.jpg') }}" alt="" class="avatar-md rounded-circle">
+            </div>
+            <div class="mt-3">
+                <h4 class="font-size-16 mb-1">Name Here</h4>
+                <span class="text-muted"><i class="ri-record-circle-line align-middle font-size-14 text-success"></i> Online</span>
+            </div>
+        </div>
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title">Menu</li>
+
+                <li>
+                    <a href="#" class="waves-effect">
+                        <i class="ri-dashboard-line"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+                <li class="menu-title">Admin</li>
+
+                <li>
+                    <a href="#" class="waves-effect">
+                        <i class="ri-group-line"></i>
+                        <span>User</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
