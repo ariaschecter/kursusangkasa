5b1beae92e387a8fddb152f51a7aafd91a38ac7e
ariaschecter <aria.maulana.m@gmail.com>
2023-01-15 19:45:56 +0700

    [PATCH] Init Database

---
+++ app/Http/Controllers/SubCourseController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubCourseController extends Controller
+{
+    //
+}
--- database/migrations/2023_01_15_124106_create_courses_table.php
+++ database/migrations/2023_01_15_124106_create_courses_table.php
@@ -13,16 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role');
-            $table->string('affiliate_id');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('courses');
     }
 };
