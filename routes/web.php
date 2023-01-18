d5b7aa489a37fc319285278a403eb1e98697fceb
ariaschecter <aria.maulana.m@gmail.com>
2023-01-18 12:07:33 +0700

    [INIT] Init Frontend Template

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('frontend.index');
+});
+
+Route::get('/sidebar', function () {
+    return view('frontend.sidebar');
+});
+
 Route::get('/dashboard', function () {
     return view('admin.index');
 })->name('dashboard');
