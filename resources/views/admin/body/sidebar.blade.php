7235d959e18761b7b329aef67bd7e538ff255425
ariaschecter <aria.maulana.m@gmail.com>
2023-02-09 17:56:09 +0700

    [PATCH] Change Leaderboard icon

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -30,7 +30,7 @@
 
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="ri-mail-send-line"></i>
+                        <i class="ri-trophy-line"></i>
                         <span>Leaderboard</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
