a93bb71bf521bf9dd76bca2726e086ac0a5d590a
ariaschecter <aria.maulana.m@gmail.com>
2023-02-09 17:32:37 +0700

    [PATCH] Affiliate Controller Title Done

--- app/Http/Controllers/AffiliateController.php
+++ app/Http/Controllers/AffiliateController.php
@@ -10,16 +10,19 @@ class AffiliateController extends Controller
 {
     public function affiliate_all() {
         $users = User::orderBy('created_at', 'DESC')->get();
-        return view('admin.affiliate.index', compact('users'));
+        $title = 'All Affiliate';
+        return view('admin.affiliate.index', compact('users', 'title'));
     }
 
     public function index() {
         $users = User::where('affiliate_id', Auth::id())->orderBy('created_at', 'DESC')->get();
-        return view('admin.affiliate.affiliate', compact('users'));
+        $title = 'My Affiliate';
+        return view('admin.affiliate.affiliate', compact('users', 'title'));
     }
 
     public function user_index() {
         $users = User::where('affiliate_id', Auth::id())->orderBy('created_at', 'DESC')->get();
-        return view('user.affiliate.affiliate', compact('users'));
+        $title = 'My Affiliate';
+        return view('user.affiliate.affiliate', compact('users', 'title'));
     }
 }
