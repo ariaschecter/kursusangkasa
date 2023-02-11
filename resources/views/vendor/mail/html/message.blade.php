23ca9613f6b87e1165f770db01b82e31b3b063fa
ariaschecter <aria.maulana.m@gmail.com>
2023-02-11 18:23:21 +0700

    [PATCH] Logo in Authenticate Mail

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
 {{-- Header --}}
 <x-slot:header>
 <x-mail::header :url="config('app.url')">
-{{ config('app.name') }}
+    <img src="{{ asset('frontend/assets/img/logo/logo.png') }}" class="logo" alt="Logo">
 </x-mail::header>
 </x-slot:header>
 
